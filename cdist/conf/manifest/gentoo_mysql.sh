b81b66a452b8d4bafac925dcb69d4f9f31237ea9
Steven Lai <steven.lai@liricco.com>
2014-12-18 15:54:42 +0800

    Create .my.cnf separately

--- cdist/conf/manifest/gentoo_mysql.sh
+++ cdist/conf/manifest/gentoo_mysql.sh
@@ -1,4 +1,9 @@
 . $__manifest/gentoo_base.sh
 __package dev-db/mysql --state present
 require="__package/dev-db/mysql" __mysql_gentoo_config
-require="__mysql_gentoo_config" __mysql_database "asterisk" --name "asterisk" --user "asterisk" --password "asterisk"
+password="$(echo "$RANDOM"|md5sum|cut -c-10)"
+__file /root/.my.cnf --source - << EOF
+[client]
+password="$password"
+EOF
+require="__file/root/.my.cnf" __mysql_database "asterisk" --name "asterisk" --user "asterisk" --password "asterisk"
