aa19d81a2b6b11c32f492cade76c3447c2a4823c
Steven Lai <steven.lai@liricco.com>
2014-12-18 08:00:42 +0000

    Remove the extra space

--- cdist/conf/manifest/gentoo_mysql.sh
+++ cdist/conf/manifest/gentoo_mysql.sh
@@ -7,4 +7,4 @@ password="$password"
 EOF
 require="__package/dev-db/mysql __file/root/.my.cnf" __mysql_gentoo_config
 require="__mysql_gentoo_config" __process /usr/sbin/mysqld --state present --start "/etc/init.d/mysql start" --stop "/etc/init.d/mysql stop"
-require="__process /usr/sbin/mysqld" __mysql_database "asterisk" --name "asterisk" --user "asterisk" --password "asterisk"
+require="__process/usr/sbin/mysqld" __mysql_database "asterisk" --name "asterisk" --user "asterisk" --password "asterisk"
