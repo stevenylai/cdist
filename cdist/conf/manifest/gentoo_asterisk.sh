7ef34d5c482593cc5b3d79095a191a7ecc3f67e6
Steven Lai <steven.lai@liricco.com>
2014-12-22 16:24:11 +0800

    Asterisk config better off to be singleton

--- cdist/conf/manifest/gentoo_asterisk.sh
+++ cdist/conf/manifest/gentoo_asterisk.sh
@@ -1,4 +1,4 @@
 . $__manifest/gentoo_base.sh
 __package net-misc/asterisk --state present
 mysql_password=$(cat "${HOME}/.liricco/${env}/mysql/password")
-require="__package/net-misc/asterisk" __asterisk_config $env --mysql_password $mysql_password
+require="__package/net-misc/asterisk" __asterisk_config --mysql_password $mysql_password
