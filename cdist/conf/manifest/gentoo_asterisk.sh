aa3c691a2c6324546847a8a1f80fc4691fce4c02
Steven Lai <steven.lai@liricco.com>
2014-12-22 14:31:02 +0800

    Add db stuff to asterisk

--- cdist/conf/manifest/gentoo_asterisk.sh
+++ cdist/conf/manifest/gentoo_asterisk.sh
@@ -1,4 +1,5 @@
 . $__manifest/gentoo_base.sh
 __package net-misc/asterisk --state present
 require="__package/net-misc/asterisk" __asterisk_config
-__asterisk_config
+mysql_password=$(cat "${HOME}/.liricco/${env}/mysql/password")
+__asterisk_config --mysql_password=$mysql_password
