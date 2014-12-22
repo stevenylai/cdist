ea28acb3c269e179061e591df7c99470650c61c4
Steven Lai <steven.lai@liricco.com>
2014-12-22 16:06:12 +0800

    No need for equation

--- cdist/conf/manifest/gentoo_asterisk.sh
+++ cdist/conf/manifest/gentoo_asterisk.sh
@@ -2,4 +2,4 @@
 __package net-misc/asterisk --state present
 require="__package/net-misc/asterisk" __asterisk_config
 mysql_password=$(cat "${HOME}/.liricco/${env}/mysql/password")
-__asterisk_config --mysql_password=$mysql_password
+__asterisk_config --mysql_password $mysql_password
