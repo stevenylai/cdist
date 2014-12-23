a30fc6c8977d34dfe9b4e633ce69fb4c67977b63
Steven Lai <steven.lai@liricco.com>
2014-12-23 23:03:12 +0800

    Set sphinx as a required package for couchdb-python

--- cdist/conf/manifest/gentoo_pyhub_base.sh
+++ cdist/conf/manifest/gentoo_pyhub_base.sh
@@ -1,7 +1,8 @@
 . $__manifest/gentoo_base.sh
+__package dev-python/sphinx_rtd_theme --state present
 __package dev-python/tox --state present
 __package www-servers/tornado --state present
-__package dev-python/couchdb-python --state present
+require="__package/dev-python/sphinx_rtd_theme" __package dev-python/couchdb-python --state present
 __package sci-libs/scipy --state present
 __package sci-libs/scikits_learn --state present
 __package dev-python/jsonschema --state present
@@ -10,5 +11,4 @@ __package dev-python/pymysql --state present
 __package dev-python/nosexcover --state present
 __package dev-python/pytest-cov --state present
 __package dev-python/pep8 --state present
-__package dev-python/sphinx_rtd_theme --state present
 
